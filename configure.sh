e54e8f431529251a024d2ae6a7300a7a0e028c2c
Shreyas <69378056+shreyaspranav@users.noreply.github.com>
2026-02-23 22:38:05 +0530

    chore: Removed configure.sh

--- configure.sh
+++
@@ -1,5 +0,0 @@
-export ARM_GCC=/usr/bin/arm-none-eabi-gcc
-export ARM_GXX=/usr/bin/arm-none-eabi-g++
-
-cmake -DCMAKE_C_COMPILER:FILEPATH=$ARM_GCC -DCMAKE_CXX_COMPILER:FILEPATH=$ARM_GXX -S . -B build/
-bear -- cmake --build build/
